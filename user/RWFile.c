d27f31be44e5267d00973440d3d17f074d4f89dc
2020303343 <2621939373@qq.com>
2023-01-15 14:16:29 +0800

    test performance

--- user/RWFile.c
+++ user/RWFile.c
@@ -9,23 +9,33 @@ const char* file_path[4] = {"dev_tty0/test.txt",
 
 
 int main(int arg, char *argv[])  {
-    for (int i = 0; i < 4; i++)
+    printf("[");
+    for (int j = 0; j < 78; j ++ )
     {
-        int tar = i % 4;
-        char read_buf[128];
-        char write_buf[128] = "Never gonna give you up!";
+        for (int i = 0; i < 400; i++)
+        {
+            int tar = i % 4;
+            char read_buf[128];
+            char write_buf[128] = "Never gonna give you up!";
 
-        int fd = open(file_path[tar], O_RDWR);
-        read(fd, read_buf, 128);
-        printf("%s\n", read_buf);
+            int fd = open(file_path[tar], O_RDWR);
+            read(fd, read_buf, 128);
+            // printf("%s\n", read_buf);
+            close(fd);
 
-        fd = open(file_path[tar], O_RDWR);
-        write(fd, write_buf, 128);
+            fd = open(file_path[tar], O_RDWR);
+            write(fd, write_buf, 128);
+            close(fd);
 
-        fd = open(file_path[tar], O_RDWR);
-        read(fd, read_buf, 128);
-        printf("%s\n", read_buf); 
+            fd = open(file_path[tar], O_RDWR);
+            read(fd, read_buf, 128);
+            close(fd);
+            // printf("%s\n", read_buf); 
+        }
+        printf("*");
     }
+    printf("]");
+    printf("The test has been finished!\n");
     
     // close(fd);
     return 0;
