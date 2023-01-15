837ec4d3c6a305a262faf557fe82c6f85d6bcae0
2020303343 <2621939373@qq.com>
2023-01-15 12:23:58 +0800

    add LRU list

--- user/RWFile.c
+++ user/RWFile.c
@@ -9,24 +9,6 @@ const char* file_path[4] = {"dev_tty0/test.txt",
 
 
 int main(int arg, char *argv[])  {
-    // printf("begin to read file\n");
-    // int fd = open(file_path, O_RDWR);
-
-    // __clock_t begin = 
-    // if (fd == -1){
-    //     printf("error occurred can not find the target file\n");
-    //     return 0;
-    // } else {
-    //     printf("successfully open the target file\n");
-    // }
-
-    // int fd_2 = open(file_path, O_RDWR);
-    // if (fd_2 == -1) {
-    //     printf("error occurred\n")        ;
-    // } else {
-    //     printf("second open the file found the target\n");
-    // }
-    
     for (int i = 0; i < 4; i++)
     {
         int tar = i % 4;
@@ -35,14 +17,14 @@ int main(int arg, char *argv[])  {
 
         int fd = open(file_path[tar], O_RDWR);
         read(fd, read_buf, 128);
-        // printf("%s\n", read_buf);
+        printf("%s\n", read_buf);
 
         fd = open(file_path[tar], O_RDWR);
         write(fd, write_buf, 128);
 
         fd = open(file_path[tar], O_RDWR);
         read(fd, read_buf, 128);
-        // printf("%s\n", read_buf); 
+        printf("%s\n", read_buf); 
     }
     
     // close(fd);
